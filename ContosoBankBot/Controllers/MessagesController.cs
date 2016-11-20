be7f17ed2252f031bf96699d8c99024211412f7a
Aleksei <lyoha.proto@gmail.com>
2016-11-20 23:46:55 +1300

    Draft implementation of states.

--- ContosoBankBot/Controllers/MessagesController.cs
+++ ContosoBankBot/Controllers/MessagesController.cs
@@ -1,41 +1,67 @@
 ﻿using System;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
-using Newtonsoft.Json;
+
 
 namespace ContosoBankBot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        //private UserInfo _interlocutorUserInfo;
+
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            StateClient stateClient = activity.GetStateClient();
+            BotData userData = stateClient.BotState.GetUserData(activity.ChannelId, activity.From.Id);
+            bool greetingMessageWasShown = userData.GetProperty<bool>("GreetingMessageWasShown");
+            string preferredUserName = userData.GetProperty<string>("PreferredName") ?? activity.From.Name;
+
+            Activity reply = activity.CreateReply("Wrong command");
+
             if (activity.Type == ActivityTypes.Message)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                if (!greetingMessageWasShown)
+                {
+                    reply = activity.CreateReply($@"Hello, {preferredUserName}!
+                                                            I am the Controso Bank Bot!
+                                                            I can tell you the latest news from Contoso Bank, show exchange rates, notify about your transactions and do other things!");
+                    //connector.Conversations.ReplyToActivityAsync(reply);
+                    userData.SetProperty<bool>("GreetingMessageWasShown", true);
+                    stateClient.BotState.SetUserData(activity.ChannelId, activity.From.Id, userData);
+                }
+                else
+                {
+                    reply = activity.CreateReply($@"{preferredUserName}, You can use standard commands or turn on language recognition mode");
+                }
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                if (activity.Text == "/deleteprofile")
+                {
+                    stateClient.BotState.DeleteStateForUser(activity.ChannelId, activity.From.Id);
+                    reply = activity.CreateReply("User data was removed");
+                }
+
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                // return our reply to the user 
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
             {
                 HandleSystemMessage(activity);
             }
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
+
 
         private Activity HandleSystemMessage(Activity message)
         {
@@ -43,6 +69,7 @@ namespace ContosoBankBot
             {
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
+                //_interlocutorUserInfo.Dispose();
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
@@ -64,6 +91,60 @@ namespace ContosoBankBot
             }
 
             return null;
+        }
+    }
+
+    public class UserInfo : IDisposable
+    {
+        private readonly StateClient _stateClient;
+        private readonly BotData _userData;
+        private readonly Activity _activity;
+
+        public UserInfo(Activity message)
+        {
+            _activity = message;
+            _stateClient = _activity.GetStateClient();
+            _userData = _stateClient.BotState.GetUserData(_activity.ChannelId, _activity.From.Id);
+        }
+
+        private void SaveUserData()
+        {
+            _stateClient.BotState.SetUserData(_activity.ChannelId, _activity.From.Id, _userData);
+        }
+
+        public string PreferredName
+        {
+            get
+            {
+                return _userData.GetProperty<string>("PreferredName") ?? "Sir or Madam";
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    value = "Sir or Madam";
+                }
+                _userData.SetProperty("PreferredName", value);
+                SaveUserData();
+            }
+        }
+
+        public bool GreetingMessageWasShown
+        {
+            get
+            {
+                return _userData.GetProperty<bool>("GreetingMessageWasShown");
+            }
+            set
+            {
+                _userData.SetProperty("GreetingMessageWasShown", value);
+                SaveUserData();
+            }
+        }
+
+        public void Dispose()
+        {
+            _stateClient.BotState.DeleteStateForUser(_activity.ChannelId, _activity.From.Id);
         }
     }
 }
